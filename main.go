f750d21e3ce9459e78684336b7e5e4e1d873481f
sjdrabbani <sajid@esenteq.com>
2020-03-26 11:20:27 +0600

    Implement API for getting all the articles - Implement API for Articles /GET - Use Gorilla Mux for routing"

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
-	Title string 'json:"title"'
-	Desc string 'json:"desc"'
-	Content string 'json:"content"'
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {	
+// Simulating DB
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	myRouter := mux.NewRouter()
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
+	//Stubbing
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
